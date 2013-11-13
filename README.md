2ef8862db4178e13ed94f44d843d8d2895b415f9
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2013-11-13 22:51:44 +0100

    -forked calc_B_st from test3c -updated readme and todo. Now follow TODO! :-)

--- README.md
+++ README.md
@@ -11,13 +11,16 @@ It contains some scripts designed to handle big matrix. The goal here is generat
 - test2.rb: reads e-f-s file and outputs integer ID based feature-vectors. The format is "feature_id \t\t element_id \t value_for_this \t element_id \t value_of_that \t ... \n". Each line shows one feature vector (one column vector, of full matrix). Output on STDOUT, with log on STDERR. Additionally, it creates "features.txt" and "elements.txt" file that maps feature/element value (string) to their index value. 
 - test3.rb: reads feature vector text file (result of test2.rb), generates a portion of the B matrix. (see memos/2nd_order_sketch.org to check what is so-called B-matrix). Output on STDOUT, with log on STDERR 
 - test3b.rb: same to test3.rb, but uses slightly different format of output, that uses gap, instead of "absolute" value of column index. Output on STDOUT, with log on STDERR 
-- test3c.rb: same to test3b; but uses "Filewrite with row index". Output on predefined file (see constant in the script), with log on STDERR 
+- test3c.rb: same to test3b; but uses "Filewrite with row index". Output on predefined file + row-from row-to numbers (see constant in the script), with log on STDERR 
+- test4.rb: (multi-thread expansion of test3. closer to real work horse) Note thta multi-thread actually works only on JRuby. 
+- test5.rb: a short scirpt that shows how to recover "relative" row index to actual "absolute" row index. 
 
-- test4.rb: (multi-thread expansion of test3. closer to real work horse) 
-
+* List of utility class
+- File_with_row_index.rb: this script holds several classes related to writing file with row indexes. By row, it means one line that ends with newline. Index, keeps position of each lines within the (text) file. Filewrite_with_row_index generates a file with one additional index file (.index); that records offset (File.pos) of each row. Class Fileread_with_row_index can access such rows very fast based on those indexes. Finally Simple_merger class gets a list of row-indexed files into one row-indexed (bigger) file. Useful for distributed running and merging. 
 
 * The main scripts: 
-(will package some of the test scripts into this, when finished. ... when?) 
+- calc_B_st.rb: calculate B matrix, single thread. (evolved from test3c)  
+- 
 
 
 * Things to consider? 
---
+++ calc_B_st.rb
@@ -0,0 +1,160 @@
+# What this script does? 
+# The script gets gets 
+# integer-ID-lized feature-vector per-line format 
+# and outputs feature x feature inner-product matrix. 
+# (rows of so-called B matrix. See /memo to check what is a B matrix)
+#
+# The script gets three arguments from command line: 
+# arg0: feature_vector file name (integer ID based feature vectors, output of test2.rb, from e-f-s file)
+# arg1: row-from  (the start row number of B matrix) 
+# arg2: row-to    (the end row number of B matrix) 
+# 
+# The script will generate a output file with OUTFILE_PREFIX + "from" + "to"
+# as line-indexed file (File_with_row_index); and outputs rows of B 
+# matrixes (one per line), starting from row-from (arg1), to row-to (arg2) 
+#
+# (To save disc space, the output has relative-index.) 
+# output format is: 
+# [row_id]\t\t[r-index]:[val]\t[r-index]:[val] ... [r-index]:[val]\n 
+# r-index is relative index, where it records delta (index - previous index) 
+
+require 'time' 
+require_relative 'File_with_row_index'
+
+# GLOBAL constant 
+OUTFILE_PREFIX = "B_matrix"
+# no longer constant #OUTFILENAME = "test3.out" 
+
+arg_check
+##
+## first, load the feature vectors. 
+t1 = Time.new 
+
+fvectors_arr = Array.new
+File.open(ARGV[0]).each_line do |line|
+  id_string, vector_string = line.split("\t\t")
+  id = id_string.to_i
+  if (fvectors_arr[id]) # sanity check, this can't exist yet. 
+    $stderr.puts("Something is wrong. duplicated feature id #{i}") 
+  end
+  
+  tmp = Array.new 
+  vector_string.split.each_with_index do |s, i|
+    if (i.even?)
+      tmp.push(s.to_i)
+    else
+      tmp.push(s.to_f)
+    end
+  end
+  fvectors_arr[id]= Hash[*tmp] # as hash 
+  #fvectors_arr[id]= tmp # as array 
+end
+
+t2 = Time.new 
+tdiff_1_2 = t2 - t1
+$stderr.puts("Loading complete: time took #{tdiff_1_2}") 
+
+# Using hash, required 7 Gb, with 20 min (1260 secs), on 17inch-macbook  
+# Using array, required x.x Gb, with MM min. 
+
+##
+## second, (with the loaded vectors), calculate from - to 
+from = ARGV[1].to_i
+to = ARGV[2].to_i
+
+# sanity chcek 
+if (from < 0)
+  from = 0
+end 
+
+if (to > fvectors_arr.size)
+  to = fvectors_arr.size - 1 
+end 
+outfilename = sprintf("%s.%07d_to_%07d.out", OUTFILE_PREFIX, from, to)
+# open Filewrite with index.  
+#indexed_file = Filewrite_with_row_index.new(OUTFILENAME)
+indexed_file = Filewrite_with_row_index.new(outfilename)
+# now use indexed_file.puts() to record. 
+
+# actual run 
+(from..to).each do |i|
+  # calculate row i of B matrix. 
+  target_h = fvectors_arr[i]
+  result_B_vec = Hash.new 
+
+  fvectors_arr.each_with_index do |h, i|
+    val = inner_product_hh(target_h, h)
+    if (val > 0.0)
+      result_B_vec[i] = val
+    end
+  end
+
+  # note that this is "not" normalized 
+  # index:val format 
+  # (unnormalized B-matrix i-th row) 
+  # +index_delta:value \t +index_delta:value ... 
+  output_line = "" 
+  temp = sprintf "%d\t\t", i
+  output_line.concat(temp)
+
+  lastkey = 0
+  result_B_vec.keys.sort.each do |k|  # note that we have keys sorted: 
+    keydelta = k - lastkey # so we can actually do this delta... 
+    temp = sprintf "%d:%.2f\t", keydelta, result_B_vec[k] # only 2 digits under . 
+    output_line.concat(temp)
+    lastkey = k
+  end
+  indexed_file.puts(output_line)
+
+  # when "recover" this output, you can use 
+  # Fileread_with_row_index class.
+  # read_row(i) method will read ith row as one line. 
+
+  # To recover "delta" index format into full "absolute" index value, 
+  # you can do this... 
+  # lastkey = 0
+  # for each splitted \t 
+  # (somehow) scan "keydelta(%d):value(%.2f)"
+  # key = lastkey + keydelta
+  # vector (hash) [key] = value 
+  # update lastkey <- key 
+
+
+end
+
+t3 = Time.new 
+tdiff_1_3 = t3 - t1
+tdiff_2_3 = t3 - t2 
+$stderr.puts("Calculated B-matrix rows from #{from} to #{to}. Took #{tdiff_2_3} seconds. Total time till now: #{tdiff_1_3}")
+
+
+BEGIN{
+
+def usage_print
+  puts "  ruby [scriptname].rb feature-vector-filename feature_id_start feature_id_end"
+  puts "  it will calculate and output rows of the B matrix, from id_start to id_end" 
+end
+
+def arg_check
+  if not (ARGV[0] and File.readable?(ARGV[0]))
+    usage_print
+    exit 
+  end
+
+  if not (ARGV[1] and ARGV[2])
+    usage_print
+    exit
+  end
+end
+
+# gets two hash (with float values), and generate inner product
+def inner_product_hh(a,b) 
+  r = 0.0 # float num to be accumulated 
+  a.keys.each do |k|
+    if b[k]
+      r = r + (a[k] * b[k]) 
+    end
+  end
+  return r 
+end 
+}
